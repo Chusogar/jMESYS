d78495db354155d5c92db0b14a42bd2e9c87637e
chusogar <chusogar@gmail.com>
2015-12-15 12:37:47 +0100

    New beta version!

--- src/jMESYS/gui/Version.java
+++ src/jMESYS/gui/Version.java
@@ -2,8 +2,8 @@ package jMESYS.gui;
 
 public class Version {
 
-	public static final String VERSION = "1.0 Beta 4";
-	public static final String RELEASE_DATE = "31-03-2015";
+	public static final String VERSION = "1.0 Beta 5";
+	public static final String RELEASE_DATE = "15-12-2015";
 	
 	public Version() {
 		super();
