9b436a65efb76e086a06e6d9e3449f9b40ed496a
chusogar <chusogar@gmail.com>
2015-12-30 10:24:36 +0100

    Blur Filter Splash Screen

--- src/jMESYS/gui/Version.java
+++ src/jMESYS/gui/Version.java
@@ -13,8 +13,8 @@ package jMESYS.gui;
  * Window - Preferences - Java - Code Style - Code Templates
  */
 public class Version {
-	public static final String VERSION="1.0 Beta 5";
-	public static final String RELEASE_DATE = "28-12-2015";
+	public static final String VERSION="1.0 Beta 6";
+	public static final String RELEASE_DATE = "30-12-2015";
 	
 	public Version() {
 		super();
